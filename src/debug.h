a63a9d95c9bf1dac3dcd651000a37e3c438dcd45
Stefan Eletzhofer <stefan.eletzhofer@nexiles.com>
2024-01-08 01:24:06 +0100

    Remove duplicate 0x

--- src/debug.h
+++ src/debug.h
@@ -5,7 +5,7 @@
 
 extern int dbg;
 
-#define  _DBGP( flag, p )  do {if (flag) fprintf( stderr, "%s(%d): %s = 0x%p\n", __FILE__, __LINE__, #p, p );} while(0);
+#define  _DBGP( flag, p )  do {if (flag) fprintf( stderr, "%s(%d): %s = %p\n", __FILE__, __LINE__, #p, p );} while(0);
 
 
 #endif // DEBUG_H_
